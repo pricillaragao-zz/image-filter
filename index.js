a80fb30eab925ffd202f67b3f2841790e401b9ba
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2020-06-06 13:27:42 -0400

    Add size and color manipulationfunctionalities

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const sliderBlur = document.getElementById("sliderblur");
 sliderBlur.addEventListener("change", function () {
   document.documentElement.style.setProperty("--blur", `${this.value}px`);
 });
+
+const sliderSize = document.getElementById("slidersize");
+
+sliderSize.addEventListener("change", function () {
+  document.documentElement.style.setProperty("--size", `${this.value}px`);
+});
+
+const changeColor = document.getElementById("change-color");
+
+changeColor.addEventListener("change", function () {
+  document.documentElement.style.setProperty("--color", this.value);
+});
