8559c2e33ded4f029fdb951e82d13bcc69f09df1
Pricilla Patricia de Aragao <pricillapatricia@gmail.com>
2020-06-17 15:12:50 -0400

    Center img input div content; modify prettier settings; start adding download button

--- index.js
+++ index.js
@@ -1,59 +1,56 @@
-const blurInput = document.getElementById("blur");
+const blurInput = document.getElementById('blur')
 
-blurInput.addEventListener("change", function () {
-  document.documentElement.style.setProperty("--blur", `${this.value}px`);
-});
+blurInput.addEventListener('change', function () {
+  document.documentElement.style.setProperty('--blur', `${this.value}px`)
+})
 
-const borderSizeInput = document.getElementById("border-size");
+const borderSizeInput = document.getElementById('border-size')
 
-borderSizeInput.addEventListener("change", function () {
-  document.documentElement.style.setProperty("--size", `${this.value}px`);
-});
+borderSizeInput.addEventListener('change', function () {
+  document.documentElement.style.setProperty('--size', `${this.value}px`)
+})
 
-const grayScaleInput = document.getElementById("grayscale");
+const grayScaleInput = document.getElementById('grayscale')
 
-grayScaleInput.addEventListener("change", function () {
-  document.documentElement.style.setProperty("--grayscale", `${this.value}%`);
-});
+grayScaleInput.addEventListener('change', function () {
+  document.documentElement.style.setProperty('--grayscale', `${this.value}%`)
+})
 
-const contrastInput = document.getElementById("contrast");
+const contrastInput = document.getElementById('contrast')
 
-contrastInput.addEventListener("change", function () {
-  document.documentElement.style.setProperty("--contrast", `${this.value}%`);
-});
+contrastInput.addEventListener('change', function () {
+  document.documentElement.style.setProperty('--contrast', `${this.value}%`)
+})
 
-const hueRotateInput = document.getElementById("hue-rotate");
+const hueRotateInput = document.getElementById('hue-rotate')
 
-hueRotateInput.addEventListener("change", function () {
-  document.documentElement.style.setProperty(
-    "--hue-rotate",
-    `${this.value}deg`
-  );
-});
+hueRotateInput.addEventListener('change', function () {
+  document.documentElement.style.setProperty('--hue-rotate', `${this.value}deg`)
+})
 
-const changeColorInput = document.getElementById("change-color");
+const changeColorInput = document.getElementById('change-color')
 
-changeColorInput.addEventListener("change", function () {
-  document.documentElement.style.setProperty("--color", this.value);
-  borderSizeInput.removeAttribute("disabled");
-});
+changeColorInput.addEventListener('change', function () {
+  document.documentElement.style.setProperty('--color', this.value)
+  borderSizeInput.removeAttribute('disabled')
+})
 
-window.addEventListener("load", function () {
-  const fileInput = document.querySelector('input[type="file"]');
+window.addEventListener('load', function () {
+  const userImgInput = document.getElementById('user-img-input')
 
-  fileInput.addEventListener("change", function () {
-    if (this.files && this.files[0]) {
-      const img = document.querySelector("img");
-      img.src = URL.createObjectURL(this.files[0]);
+  userImgInput.addEventListener('change', function () {
+    if (this.files && this.files.length > 0) {
+      const file = this.files[0]
+
+      const userImg = document.getElementById('user-img')
+      userImg.src = URL.createObjectURL(file)
+
+      const imgDownloadAnchor = document.getElementById('user-img-download')
+      const filename = file.name
+      const blob = new Blob([file])
+      const url = URL.createObjectURL(blob)
+      imgDownloadAnchor.setAttribute('download', `edited-${filename}`)
+      imgDownloadAnchor.setAttribute('href', url)
     }
-  });
-});
-
-// const downloadImageEdited = document.getElementById("btn");
-
-// document
-//   .querySelector("a")
-//   .addEventListener(
-//     "click",
-//     (event) => (event.target.href = downloadImageEdited.toDataURL())
-//   );
+  })
+})
